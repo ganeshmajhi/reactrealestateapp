707c0916cb82bda855e9cca832acfc3f2fbe0d29
ganeshmajhi <ganeshmajhi@gmail.com>
2022-01-18 23:54:56 +0530

    added compnents

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,8 @@ const Banner =({ purpose, imageUrl, title1, title2, desc1, desc2, urlName, butto
 )
 export default function Home() {
   return (
-    <div>
-    <h1>Hello World !</h1>  
+    <Box>
+     
     <Banner 
         purpose="RENT A HOME"
         title1="Rental Homes for"
@@ -33,6 +33,12 @@ export default function Home() {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
 
     />
+    <Flex flexWrap="wrap">
+      {/* fetch the property and map over them.. */}
+
+      
+
+    </Flex>
     <Banner 
         purpose="BUY A HOME"
         title1="Buy Homes for"
@@ -43,6 +49,6 @@ export default function Home() {
         urlName="/search?purpose-for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
     />
-    </div>
+    </Box>
   )
 }
