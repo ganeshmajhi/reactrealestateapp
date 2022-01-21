bb6553c53f93adb53ecdba4424fb9f91032b1d27
ganeshmajhi <ganeshmajhi@gmail.com>
2022-01-22 00:24:14 +0530

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import {Box} from '@chakra-ui/react';
+
+const Footer = () => (
+ <Box p="5" textAlign="center" color="gray.600" borderTop="1px" borderColor="gray.200">
+     2022 HomeParadise, Inc.
+ </Box>
+)
+
+export default Footer;
