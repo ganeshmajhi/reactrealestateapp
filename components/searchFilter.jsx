8e9399de1022f59c0008ad76a20aac30402b7c3b
ganeshmajhi <ganeshmajhi@gmail.com>
2022-01-23 17:00:57 +0530

    added search page

---
+++ components/searchFilter.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { Flex, Box, Text, Imput, Spinner, Icons, Select, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import {MdCancel} from "react-icons/md";
+import Image from "next/image";
+
+const SearchFilters = () => {
+    return(
+        <div>Search</div>
+
+    )
+        
+    
+
+}
+
+export default SearchFilters;
