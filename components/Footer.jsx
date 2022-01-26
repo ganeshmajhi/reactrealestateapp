8c967e538a6aca6666d0eb176bcc64e95098e60f
ganeshmajhi <ganeshmajhi@gmail.com>
2022-01-26 23:43:56 +0530

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import {Box} from '@chakra-ui/react';
 
 const Footer = () => (
  <Box p="5" textAlign="center" color="gray.600" borderTop="1px" borderColor="gray.200">
-     2022 HomeParadise, Inc. <br/> Made with love in Bangalore.
+     2022 HomeParadise, Inc. @ all rights reserved <br/> Made with love in Bangalore.
  </Box>
 )
 
